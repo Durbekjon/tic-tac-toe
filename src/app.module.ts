58bdf96a2d8583988579a60cbd3af741d1d9b7ca
Durbek <saydaliyevdurbek0512@gmail.com>
2024-11-22 01:55:32 +0500

    fix: remove unused imports
    - Removed imports for deleted AppController and AppService
    - Cleaned up module declarations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { HealthController } from './health/health.controller';
 
@@ -14,7 +12,7 @@ import { HealthController } from './health/health.controller';
     }),
     GameModule,
   ],
-  controllers: [ HealthController],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
